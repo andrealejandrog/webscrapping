0d7bd4efa8f61df020b17518706b8ac4c1e40ba6
Andres Alejandro Gonzalez <89698289+andrealejandrog@users.noreply.github.com>
2023-07-29 13:58:24 -0600

    finish the scrapping and created the dataframe with the information

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 import time
 
+import pandas as pd
+
 website = "https://www.adamchoi.co.uk/teamgoals/detailed"
 
 os.environ["PATH"] += r";D:\Platzi\python\webscrapper"
@@ -25,9 +27,31 @@ countrySelection = wait.until(
 select = Select(countrySelection)
 select.select_by_visible_text("USA")
 
-matches = driver.find_elements(By.TAG_NAME, "td")
+time.sleep(15)
+
+matches = driver.find_elements(By.TAG_NAME, "tr")
+
+dates = []
+home_team = []
+score = []
+away_team = []
+home = []
 
 for match in matches:
-    print(match.text)
+    home = match.find_element(By.XPATH, "./td[1]").text
+    dates.append(home)
+    home_team.append(match.find_element(By.XPATH, "./td[2]").text)
+    score.append(match.find_element(By.XPATH, "./td[3]").text)
+    away_team.append(match.find_element(By.XPATH, "./td[4]").text)
+
+
+data = pd.DataFrame()
+data["Dates"] = dates
+data["Home Team"] = home_team
+data["Score"] = score
+data["Away Team"] = away_team
+
+data.to_excel("Data.xlsx")
+
 
 time.sleep(30)
